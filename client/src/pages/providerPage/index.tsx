f7fb50c9ad73af9714590f8fa43358c1f2a8e700
Wesley Ong <72511481+Wesleyongs@users.noreply.github.com>
2023-06-29 15:29:07 +0800

    Feat: Complete implementation of ProviderPage

--- client/src/pages/providerPage/index.tsx
+++ client/src/pages/providerPage/index.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import Sidebar from "../../components/sidebar";
-import { ApiResponse, ProviderInfo } from "../../shared/types";
+import { ApiResponse, ProviderData } from "../../shared/types";
 import styles from "./providerPage.module.css";
 
 interface ProviderPageProps {
@@ -13,50 +13,63 @@ const ProviderPage: React.FC<ProviderPageProps> = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const selectedProvider = useSelector((state: any) => state.selectedProvider);
-  const providersInfo = useSelector((state: any) => state.providersInfo);
+  const providersData = useSelector((state: any) => state.providersData);
 
-  const [selectedProviderInfo, setSelectedProviderInfo] =
-    useState<ProviderInfo | null>(null);
+  const [selectedproviderData, setSelectedproviderData] =
+    useState<ProviderData | null>(null);
 
   useEffect(() => {
-    // Update the selectedProviderInfo state using the selectedProvider and providersInfo
+    // Update the selectedproviderData state using the selectedProvider and providersData
     console.log(selectedProvider);
-    console.log(providersInfo);
-    if (selectedProvider && providersInfo) {
-      setSelectedProviderInfo(providersInfo[selectedProvider] || null);
+    console.log(providersData);
+    if (selectedProvider && providersData) {
+      setSelectedproviderData(providersData[selectedProvider] || null);
     }
-  }, [selectedProvider, providersInfo]);
+  }, [selectedProvider, providersData]);
 
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
-        <div>Banner goes here</div>
-        <div>
-          <h2>Description</h2>
-          <p>{selectedProviderInfo?.description}</p>
+        <div className={styles.hero}>
+          <img
+            className="logo"
+            src={selectedproviderData?.info["x-logo"].url}
+            alt="Provider Logo"
+          />
+          <h1>{selectedproviderData?.info.title}</h1>
         </div>
         <div>
           <h2>Description</h2>
-          <p>Descriptions goes here</p>
+          <p>{selectedproviderData?.info.description}</p>
         </div>
         <div>
-          <h2>Description</h2>
+          <h2>Swagger</h2>
+          <p>{selectedproviderData?.swaggerUrl}</p>
+        </div>
+        <div>
+          <h2>Contact</h2>
           <table>
             <tr>
-              <td>Row 1</td>
-              <td>Row 1</td>
+              <td>Email</td>
+              <td>
+                {selectedproviderData?.info?.contact?.email ?? "Missing data"}
+              </td>
             </tr>
             <tr>
-              <td>Row 2</td>
-              <td>Row 2</td>
+              <td>Name</td>
+              <td>
+                {selectedproviderData?.info?.contact?.name ?? "Missing data"}
+              </td>
             </tr>
             <tr>
-              <td>Row 3</td>
-              <td>Row 3</td>
+              <td>URL</td>
+              <td>
+                {selectedproviderData?.info?.contact?.url ?? "Missing data"}
+              </td>
             </tr>
           </table>
         </div>
-        <button onClick={() => navigate("/")}>Click me</button>
+        <button onClick={() => navigate("/")}>Explore more APIs</button>
       </div>
     </div>
   );
